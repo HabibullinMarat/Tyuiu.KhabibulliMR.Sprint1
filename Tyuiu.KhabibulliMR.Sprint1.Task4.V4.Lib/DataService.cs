bad31b1ae7194188007360102627f8d3a83dd5a3
habibullinm52@gmail.com <habibullinm52@gmail.com>
2024-10-15 17:55:07 +0500

    Task4

--- Tyuiu.KhabibulliMR.Sprint1.Task4.V4.Lib/DataService.cs
+++ Tyuiu.KhabibulliMR.Sprint1.Task4.V4.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KhabibulliMR.Sprint1.Task4.V4.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = (1 + x * y) / (Math.Abs(x+2));
+            double res = Math.Round((1 + x * y) / (Math.Abs(x+2)),3);
             return res;
         }
     }
