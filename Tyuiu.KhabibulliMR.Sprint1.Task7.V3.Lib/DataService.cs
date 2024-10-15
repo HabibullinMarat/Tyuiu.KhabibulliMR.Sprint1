eda9e444f119b64938ffa252b907676aa73b7838
habibullinm52@gmail.com <habibullinm52@gmail.com>
2024-10-15 19:46:03 +0500

    Task7

---
+++ Tyuiu.KhabibulliMR.Sprint1.Task7.V3.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KhabibulliMR.Sprint1.Task7.V3.Lib
+{
+    public class DataService : ISprint1Task7V3
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = Math.Round((3 + Math.Exp(y - 1)) / (1 + (x * x) * Math.Abs(y - Math.Tan(x))),3);
+            return z;
+        }
+    }
+}
---
+++ Tyuiu.KhabibulliMR.Sprint1.Task7.V3/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KhabibulliMR.Sprint1.Task7.V3.Lib;
+
+namespace Tyuiu.KhabibulliMR.Sprint1.Task7.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                                 ");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
